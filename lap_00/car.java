2f9b9fcc4564c87e28cbf43654abee506924370c
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2026-02-09 19:56:26 +0100

    feat: add tp file

---
+++ lap_00/car.java
@@ -0,0 +1,24 @@
+public class car {
+
+    public static void main(String[] args) {
+        String name = "406";
+
+        String type = "peugeot";
+        String engine = "hdi";
+        String color = "gray";
+        int maxSpeed = 197;
+
+        System.out.println(
+            "this car has type of: " +
+                type +
+                " with name: " +
+                name +
+                " has engine: " +
+                engine +
+                " with color: " +
+                color +
+                " and its maximum speed: " +
+                maxSpeed
+        );
+    }
+}
