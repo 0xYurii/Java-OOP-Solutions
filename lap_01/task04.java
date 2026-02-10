54925b0bb78aa5de6d67abce36cb0ba9fec1dd7d
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2026-02-10 12:41:04 +0100

    feat: update task04

--- lap_01/task04.java
+++ lap_01/task04.java
@@ -9,7 +9,9 @@ public class task04 {
             System.out.println("enter number from 0 to 9999");
             return;
         }
+
         int[] list_of_cents = { 200, 100, 50, 20, 10, 5, 2, 1 };
+
         for (int i = 0; i < list_of_cents.length; i++) {
             int piece = cent / list_of_cents[i];
             System.out.println(list_of_cents[i] + "c: " + piece + ";");
